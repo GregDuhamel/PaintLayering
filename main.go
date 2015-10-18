ec49bf7079a016f85afcc6aed8e483684f6b4948
GregDuhamel <gregory.duhamel@gmail.com>
2015-10-18 16:54:22 +0200

    Add JSON file GET through URL in YAML config file. Map it to a GO struct.

--- main.go
+++ main.go
@@ -10,16 +10,43 @@ import (
 	"fmt"
 	"time"
 	"strconv"
+	"net/http"
+	"encoding/json"
+	"os"
 )
 
 type Config struct {
 	Descritption string
+	MongoDB map[string]string
 	Url map[string]string
+}
+
+type envId struct {
+	CustomLimitMessage	string `json:"customLimitMessage"`
+	Skus				[]paintId `json:"skus"`
+	Dangerous 			bool `json:"dangerous"`
+    Hazardous 			bool `json:"hazardous"`
+	LimitedQuantity 	uint8 `json:"limitedQuantity"`
+	ProductId			string `json:"productId"`
+}
+
+type paintId struct {
+	Id string `json:"id"`
+	AvailabilityStatus string `json:"availabilityStatus"`
+	Title string `json:"title"`
+	Price string `json:"price"`
+	ImageName string `json:"imageName"`
+	ProductTitle string `json:"productTitle"`
+	DisplayName string `json:"displayName"`
+	Swatch string `json:"swatch"`
+	Dangerous bool `json:"dangerous"`
+	ProductType string `json:"productType"`
+	ProductId string `json:"productId"`
 }
 
 func handleError(e error) {
 	if e != nil {
-		log.Fatalf("error: %v", e)
+		log.Fatalf("[ERROR] - %v", e)
 	}
 	return
 }
@@ -35,35 +62,43 @@ func (e *plError) Error() string {
 }
 
 func buildURL(conf *Config) string {
-	if (len(conf.Url["login"]) == 0) {
-		conf.Url["login"] = "Guest"
+	if (len(conf.MongoDB["login"]) == 0) {
+		conf.MongoDB["login"] = "Guest"
 	}
-	if (len(conf.Url["password"]) == 0) {
-		conf.Url["password"] = "Guest"
+	if (len(conf.MongoDB["password"]) == 0) {
+		conf.MongoDB["password"] = "Guest"
 	}
-	if (len(conf.Url["hostname"]) == 0) {
-		conf.Url["hostname"] = "localhost"
+	if (len(conf.MongoDB["hostname"]) == 0) {
+		conf.MongoDB["hostname"] = "localhost"
 	}
-	if (len(conf.Url["port"]) == 0) {
-		conf.Url["port"] = "27017"
+	if (len(conf.MongoDB["port"]) == 0) {
+		conf.MongoDB["port"] = "27017"
 	}
-	if (len(conf.Url["database"]) == 0) {
-		conf.Url["database"] = "PaintLayering"
+	if (len(conf.MongoDB["database"]) == 0) {
+		conf.MongoDB["database"] = "PaintLayering"
 	}
-	if (len(conf.Url["options"]) == 0) {
-		conf.Url["options"] = ""
+	if (len(conf.MongoDB["options"]) == 0) {
+		conf.MongoDB["options"] = ""
 	}
-	conn := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s?%s", conf.Url["login"], conf.Url["password"], conf.Url["hostname"], conf.Url["port"], conf.Url["database"], conf.Url["options"])
+	conn := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s?%s", conf.MongoDB["login"], conf.MongoDB["password"], conf.MongoDB["hostname"], conf.MongoDB["port"], conf.MongoDB["database"], conf.MongoDB["options"])
 	
 	return conn
+}
+
+func getJson(url string, target interface{}) error {
+    r, err := http.Get(url)
+	handleError(err)
+    defer r.Body.Close()
+
+    return json.NewDecoder(r.Body).Decode(target)
 }
 
 func main() {
 	var filename string
 	var conf Config
+	var e []envId
 	
 	flag.StringVar(&filename, "conf", "", "a YAML config file")
-	
 	flag.Parse()
 	
 	if len(filename) == 0 {
@@ -81,15 +116,15 @@ func main() {
 	
 	url := buildURL(&conf)
 	
-	if (len(conf.Url["description"]) != 0) {
-		fmt.Println(conf.Url["description"])		
+	if (len(conf.MongoDB["description"]) != 0) {
+		log.Println(conf.MongoDB["description"])		
 	}
 	
-	if (len(conf.Url["timeout"]) == 0) {
-		conf.Url["timeout"] = "15"
+	if (len(conf.MongoDB["timeout"]) == 0) {
+		conf.MongoDB["timeout"] = "15"
 	}
 	
-	timeint, err := strconv.Atoi(conf.Url["timeout"])
+	timeint, err := strconv.Atoi(conf.MongoDB["timeout"])
 	handleError(err)
 
 	timeout := time.Duration(timeint)
@@ -100,6 +135,12 @@ func main() {
 	defer session.Close()
 	
 	session.SetMode(mgo.Monotonic, true)
-
-	return
-}
+	
+	err = getJson(conf.Url["Layer"], &e)
+	handleError(err)
+	
+	fmt.Printf("Id: %v\n", e[0].Skus[0].Id)
+	fmt.Printf("ProductName: %v\n", e[0].Skus[0].Title)
+	
+	os.Exit(0)
+}	
